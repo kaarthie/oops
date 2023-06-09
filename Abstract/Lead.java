d845fdf91aa2f16b24f08636c3f82bc0723b4095
kaarthie <89959138+kaarthie@users.noreply.github.com>
2023-06-09 08:30:05 +0530

    Add files via upload

---
+++ Abstract/Lead.java
@@ -0,0 +1,28 @@
+package Abstract;
+
+public abstract class Lead {
+    abstract void planNo(int n);
+    abstract void details(String domain,String[] members,int estimate);
+}
+class Team extends Lead{
+
+    @Override
+    void planNo(int n) {
+        System.out.println("plan no "+n);
+    }
+
+    @Override
+    void details(String domain, String[] members, int estimate) {
+        System.out.println("The domain is "+domain+"\nThe members are");
+        for (int i = 0; i < members.length; i++) {
+            System.out.println(i+1+". "+members[i]);
+        }
+        System.out.println(estimate+" is the appx estimation of our project");
+    }
+
+    public static void main(String[] args) {
+        Team a=new Team();
+        a.planNo(5);
+        a.details("MACHINE LEARNING",new String[]{"Ravi","Kumar"},100000);
+    }
+}
