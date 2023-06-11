1bc8ba96016c8671f9cb761949cd69f5eb9afc10
kaarthie <89959138+kaarthie@users.noreply.github.com>
2023-06-11 11:39:38 +0530

    Add files via upload

---
+++ InheritanceBook.java
@@ -0,0 +1,62 @@
+package Inheritance;
+
+public class InheritanceBook {
+    private String title;
+    private String author;
+    private int year;
+
+    public InheritanceBook(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void displayBookInfo() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Year: " + year);
+    }
+}
+
+class Main5 {
+    public static void main(String[] args) {
+        InheritanceBook book = new InheritanceBook("To Kill a Mockingbird", "Harper Lee", 1960);
+
+        System.out.println("Original Book Info:");
+        book.displayBookInfo();
+
+        book.setYear(1962);
+        System.out.println("\nUpdated Book Info:");
+        book.displayBookInfo();
+
+        book.setTitle("The Great Gatsby");
+        book.setAuthor("F. Scott Fitzgerald");
+        System.out.println("\nModified Book Info:");
+        book.displayBookInfo();
+    }
+}
+
