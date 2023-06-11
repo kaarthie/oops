db5e4c15adcfa5cf73e7074cb38d58950d4bba32
kaarthie <89959138+kaarthie@users.noreply.github.com>
2023-06-11 09:31:31 +0530

    Add files via upload

---
+++ InheritanceCar.java
@@ -0,0 +1,47 @@
+package Inheritance;
+
+class InheritanceCar {
+    private String brand;
+    private String model;
+    private int year;
+
+    public InheritanceCar(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void startEngine() {
+        System.out.println("Starting the engine of the " + brand + " " + model);
+    }
+}
+class Main3 {
+    public static void main(String[] args) {
+        InheritanceCar car = new InheritanceCar("Maruthi Suzuki","Swift",2018);
+
+        System.out.println("Brand: " + car.getBrand());
+        System.out.println("Model: " + car.getModel());
+        System.out.println("Year: " + car.getYear());
+
+        car.setYear(2022);
+        System.out.println("Updated Year: " + car.getYear());
+
+        car.startEngine();
+    }
+}
