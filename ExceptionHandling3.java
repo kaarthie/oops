84715dfcd2ac936b1c0e53ea532144ae622d93f2
kaarthie <89959138+kaarthie@users.noreply.github.com>
2023-06-19 20:56:33 +0530

    Update ExceptionHandling3.java

--- ExceptionHandling3.java
+++ ExceptionHandling3.java
@@ -1,5 +1,10 @@
 public class ExceptionHandling3 {
     public static void main(String[] args) {
-
+        try{
+            throw new Exception("custom exception");
+        }
+        catch(Exception e){
+            System.out.println("From custom catch");
+        }
     }
 }
