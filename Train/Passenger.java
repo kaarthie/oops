5145a2a31cc05298e7f36765daff1a9e70154c6a
kaarthie <89959138+kaarthie@users.noreply.github.com>
2023-08-02 12:25:24 +0530

    Update Passenger.java

--- Train/Passenger.java
+++ Train/Passenger.java
@@ -3,19 +3,23 @@ package Train;
 import java.util.*;
 
 public class Passenger {
+
     public static void main(String[] args) {
+        System.out.println("************TRAIN TICKET BOOKING************");
         Scanner in=  new Scanner(System.in);
         boolean con = true ;
         int n;
         while(con){
-            System.out.println("1. Book\n2. Print Booked tickets\n3. Available Tickets\n4. Cancel ");
+            System.out.println("1. Book\n2. Print Booked tickets\n3. Available Tickets\n4. Cancel \n");
             n=in.nextInt();
             switch(n){
                 case 1:{
-                    System.out.println("Enter Details : ");
+                    System.out.print("Enter Name : ");
                     String name=in.next();
+                    System.out.print("Enter Age : ");
                     int age=in.nextInt();
                     in.nextLine();
+                    System.out.print("Enter berth : ");
                     char c=in.next().charAt(0);
                     User user=new User(name,age,c);
                     Details.addRes(user);
@@ -32,7 +36,6 @@ public class Passenger {
                 case 4:{
                     System.out.println("Enter id to Cancel : ");
                     Details.cancel(in.nextInt());
-                    con=false;
                     break;
                 }
             }
